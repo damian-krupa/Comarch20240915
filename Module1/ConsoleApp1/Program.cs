ecb70becc5905c59c8c7e7bb241de3b64921f933
damian-krupa <damian.k.krupa@gmail.com>
2024-09-15 13:03:38 +0200

    first console app

--- Module1/ConsoleApp1/Program.cs
+++ Module1/ConsoleApp1/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Witaj, jak masz na imię?!");
+            string name = Console.ReadLine();
+            Console.Write("Podaj wiek: ");
+            string sAge = Console.ReadLine();
+            int age = Convert.ToInt32(sAge);
+
+            Console.WriteLine($"Witaj {name}, masz {age} lat ");
+
+            int currentYear = DateTime.Now.Year;
+
+            Console.WriteLine($"Urodziłeś się w {currentYear - age} roku");
+            Console.WriteLine("Naciśnij dowolny klawisz aby zakończyć...");
+            Console.ReadKey();
         }
     }
 }
